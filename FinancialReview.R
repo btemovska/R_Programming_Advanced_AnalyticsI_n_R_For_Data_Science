1536f8321017df4febab879a4b0e03a48b06d99b
btemovska <btemovska@gmail.com>
2021-01-01 12:53:22 -0500

    section 2

---
+++ FinancialReview.R
@@ -0,0 +1,10 @@
+getwd()
+setwd("C:\\Users\\btemo\\OneDrive\\Documents\\Udemy\\R Programming Advanced Analytics In R For Data Sciene\\Section 2 Data Preparation")
+getwd()
+fin<-read.csv("P3-Future-500-The-Dataset.csv")
+
+
+
+
+
+
