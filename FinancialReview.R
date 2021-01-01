71cb878b02f76d597495407c905f53c45b0d8092
btemovska <btemovska@gmail.com>
2021-01-01 13:05:31 -0500

    section 2

--- FinancialReview.R
+++ FinancialReview.R
@@ -2,9 +2,12 @@ getwd()
 setwd("C:\\Users\\btemo\\OneDrive\\Documents\\Udemy\\R Programming Advanced Analytics In R For Data Sciene\\Section 2 Data Preparation")
 getwd()
 fin<-read.csv("P3-Future-500-The-Dataset.csv")
+head(fin, 10)
+tail(fin)
+str(fin)
 
-
-
-
-
+#changing from non-factor to factor:
+#ID has integer data type, this needs to be categorical variable
+fin$ID <- factor(fin$ID)
+fin$Inception <-factor(fin$Inception)
 
