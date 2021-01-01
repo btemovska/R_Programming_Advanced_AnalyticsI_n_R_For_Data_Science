fc96b23798152f5c9e69214ee56b18f26c69a554
btemovska <btemovska@gmail.com>
2021-01-01 16:42:37 -0500

    section 2

--- FinancialReview.R
+++ FinancialReview.R
@@ -85,7 +85,89 @@ tail(fin)
 
 #----------------Part 4 - Replacing Missing Data
 #4A - Replacing Missing Data: Factual Analysis Method
+fin[!complete.cases(fin),]
+#now focusing on State column
+fin[is.na(fin$State),] #subset of rows with NA values in State column (there are 4 rows)
+fin[is.na(fin$State) & fin$City == "New York",] 
+fin[is.na(fin$State) & fin$City == "New York", "State"] <- "NY" 
+                      #where State is NA and City == New York, we want to address "State" and replace it with NY
+#check:
+fin[c(11, 377),]
+
+fin[is.na(fin$State),]
+fin[is.na(fin$State) & fin$City == "San Francisco", "State"] <- "CA"
+fin[c(82, 265),]
+
+fin[!complete.cases(fin),] #now we have 5 rows to deal that have NA
+
+#4B - Replacing Missing Data: Median Imputation Method (Part 1)
+#fixing the Employee Column
+#step 1 find retail industry
+med_emp_retail <- median(fin[fin$Industry == "Retail","Employees"], na.rm = TRUE) #28
+fin[is.na(fin$Employees) & fin$Industry=="Retail","Employees"] <- med_emp_retail
+#check
+fin[3,]
+#step# find the Financial Services industry
+med_emp_FS <- median(fin[fin$Industry == "Financial Services", "Employees"], na.rm = TRUE) 
+med_emp_FS #80
+fin[is.na(fin$Employees) & fin$Industry=="Financial Services", "Employees"] <- med_emp_FS
+fin[330,]
+
+fin[!complete.cases(fin),] #now we have 4 rows to deal that have NA
+
+#4C - Replacing Missing Data: Median Imputation Method (Part 2)
+#fixing Growth Column
+med_growth_constr <- median(fin[fin$Industry == "Construction", "Growth"], na.rm = TRUE)
+med_growth_constr #10
+fin[is.na(fin$Growth) & fin$Industry == "Construction", "Growth"] <- med_growth_constr
+#check
+fin[8,]
+
+#4C - Replacing Missing Data: Median Imputation Method (Part 3)
+#fixing Revenue Column
+med_revenue_constr <- median(fin[fin$Industry == "Construction", "Revenue"], na.rm = TRUE)
+med_revenue_constr #9055059
+fin[is.na(fin$Revenue) & fin$Industry == "Construction", "Revenue"] <- med_revenue_constr
+#check
+fin[c(8, 42),]
+#fixing Expenses column with NA values, but also where Profit is NA too
+med_exp_constr <- median(fin[fin$Industry == "Construction", "Expenses"], na.rm=TRUE)
+med_exp_constr #4506976
+fin[is.na(fin$Expenses) & fin$Industry == "Construction" & is.na(fin$Profit),]
+fin[is.na(fin$Expenses) & fin$Industry == "Construction" & is.na(fin$Profit), "Expenses"] <- med_exp_constr
+#check
+fin[c(8, 42),]
+
+fin[!complete.cases(fin),] #now we have 4 rows to deal that have NA
+
+#4D - Replacing Missing Data: Deriving Values Method
+
+#Profit = Revenue - Expenses (fixing Profit)
+fin[is.na(fin$Profit),]
+fin[is.na(fin$Profit), "Profit"] <-fin[is.na(fin$Profit), "Revenue"] - fin[is.na(fin$Profit), "Expenses"]
+fin[c(8, 42),]
+
+fin[!complete.cases(fin),] #now we have 2 rows to deal that have NA
+
+#Expenses = Revenue - Profit (fixing Expenses)
+fin[is.na(fin$Expenses),]
+fin[is.na(fin$Expenses), "Expenses"] <-fin[is.na(fin$Expenses), "Revenue"] - fin[is.na(fin$Expenses), "Profit"]
+fin[c(15, 20),]
+
+fin[!complete.cases(fin),] #now we have 1 rows to deal that have NA
 
 
+#----------------Part 5 - Visualizing results
+#A Scatterplot classified by industry showing revenue, expenses, profit
+library(ggplot2)
+p <-ggplot(data=fin)
+p + geom_point(aes(x=Revenue, y=Expenses,
+               colour=Industry, size=Profit))
+#B Scatterplot that includes industry trends for the expense
+d <-ggplot(data=fin, aes(x=Revenue, y=Expenses,
+                         colour=Industry))
+d + geom_point()+geom_smooth(fill=NA, size=1.2)
 
-
+#BoxPlots showing growth by industry
+f <- ggplot(data=fin, aes(x=Industry, y=Growth, colour = Industry))
+f + geom_jitter() + geom_boxplot(size = 1, alpha = 0.5, outlier.color=NA)
